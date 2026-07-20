1d305bd9399a9f27da448013739c46d89bf1360e
Dmitry Pavlov <dapavlov@etu.ru>
2026-07-20 20:18:07 +0300

    downloading asc2eph.f in script

--- de430/convert-de430.sh
+++ de430/convert-de430.sh
@@ -1,2 +1,4 @@
+wget --no-check-certificate https://ssd.jpl.nasa.gov/ftp/eph/planets/fortran/asc2eph.f &&
+sed -i '/PARAMETER ( NRECL = 4 )/s/^C//' asc2eph.f &&
 gfortran asc2eph.f -o asc2eph
 cat header.430_229 ascp*.430 | ./asc2eph
