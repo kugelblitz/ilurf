22d3248cd339fbc451e9d70de4a71e6d3230b412
Dmitry Pavlov <dapavlov@etu.ru>
2026-07-20 20:18:35 +0300

    EPM download script

---
+++ download-epm.sh
@@ -0,0 +1,7 @@
+for F in epm2021.bsp epm2021.tpc moonlibr_epm2021.bpc moonlibr_epm2021.tf; do   
+    wget --no-check-certificate https://ftp.iaaras.ru/pub/epm/EPM2021/SPICE/$F;
+done
+
+# add a line for Calceph to read the lunar ephemeris without problems
+echo "      OBJECT_MOON_FRAME = 'MOON_PA_EPM2021'" >> moonlibr_epm2021.tf
+
