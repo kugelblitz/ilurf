57bd24628b51cb1c3ee505338a7cf2c399bdba09
Dmitry Pavlov <dapavlov@etu.ru>
2026-07-20 21:15:50 +0300

    cosmetic

--- de430/download-de430.sh
+++ de430/download-de430.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 for n in `seq 1550 100 2550`; do
-    wget --no-check-certificate https://ssd.jpl.nasa.gov/ftp/eph/planets/ascii/de430/ascp$n.430;
-done
-wget --no-check-certificate https://ssd.jpl.nasa.gov/ftp/eph/planets/ascii/de430/header.430_229
-wget --no-check-certificate https://ssd.jpl.nasa.gov/ftp/eph/planets/fortran/asc2eph.f
+    wget -nc --no-check-certificate https://ssd.jpl.nasa.gov/ftp/eph/planets/ascii/de430/ascp$n.430;
+done;
+wget -nc --no-check-certificate https://ssd.jpl.nasa.gov/ftp/eph/planets/ascii/de430/header.430_229;
+wget -nc --no-check-certificate https://ssd.jpl.nasa.gov/ftp/eph/planets/fortran/asc2eph.f
 
